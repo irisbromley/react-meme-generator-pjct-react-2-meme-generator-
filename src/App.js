649e6d997f6747cfb8181b069fb6fa052af03d2e
Iris Bromley <iris.j.bromley@gmail.com>
2023-01-20 15:32:22 +0100

    Get imgUrl and display, update style w inline-css

--- src/App.js
+++ src/App.js
@@ -1,41 +1,93 @@
-import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+// template for the generated meme(s)
+function ImagePreview(props) {
+  console.log(props);
+  const style = {
+    width: '350px',
+    paddingTop: '20px',
+  };
+  return (
+    <img
+      style={style}
+      data-test-id="meme-image"
+      alt="meme template"
+      src={props.imageUrl}
+    />
+  );
+}
 
 export default function App() {
   const [textT, setTextT] = useState('');
   const [textB, setTextB] = useState('');
+
+  const [imageUrl, setImageUrl] = useState();
+
+  useEffect(() => {
+    fetch('https://api.memegen.link/templates')
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        console.log(result);
+        return result;
+      })
+      .then((listOfObjects) => {
+        setImageUrl(listOfObjects[0].blank);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
   return (
     <>
       <div>
         <h1>React Meme Generator</h1>
 
-        <div className="topText">
-          <label htmlFor="Top text" placeholder="What should it say here?">
-            <input
-              value={textT}
-              onChange={(event) => {
-                const topText = setTextT(event.currentTarget.value);
-                console.log('topText input', textT);
-              }}
-              name="Top text"
-            />
-          </label>
+        <div style={{ display: 'flex' }} className="topText">
+          <label htmlFor="Top text">Top Text: </label>
+          <input
+            style={{ marginLeft: 'auto', marginBottom: '10px' }}
+            value={textT}
+            onChange={(event) => {
+              const topText = textT;
+              setTextT(event.currentTarget.value);
+              console.log('topText input', event.currentTarget.value);
+            }}
+            name="Top text"
+          />
         </div>
-        <div className="memePic">Here goes the meme</div>
+        <div style={{ display: 'flex' }} className="bottomText">
+          <label htmlFor="Bottom text">Bottom Text: </label>
+          <input
+            style={{ marginLeft: 'auto', marginBottom: '10px' }}
+            value={textB}
+            onChange={(event) => {
+              const bottomText = setTextB(event.currentTarget.value);
+              console.log('bottomText input', event.currentTarget.value);
+            }}
+            name="Bottom text"
+          />
+        </div>
 
-        <div className="bottomText">
-          <label htmlFor="Bottom text" placeholder="Do you like peas?">
-            <input
-              value={textB}
-              onChange={(event) => {
-                const bottomText = setTextB(event.currentTarget.value);
-                console.log('bottomText input', textB);
-              }}
-              name="Bottom text"
-            />
-          </label>
+        <div style={{ display: 'flex' }}>
+          <label htmlFor="Bottom text">Search for an image: </label>
+          <input
+            style={{ marginLeft: 'auto' }}
+            value={textB}
+            onChange={(event) => {
+              const bottomText = setTextB(event.currentTarget.value);
+              console.log('bottomText input', event.currentTarget.value);
+            }}
+            name="Bottom text"
+          />
         </div>
-        <button>Download</button>
+
+        <ImagePreview imageUrl={imageUrl} />
+        <div style={{ display: 'flex' }}>
+          <button style={{ marginLeft: 'auto' }}>Download</button>
+        </div>
       </div>
       <div />
     </>
